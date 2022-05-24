b436a01b5aeee880cbb10af71c02944cdb925d59
kyotobear <kiytobear@gmail.com>
2022-05-23 20:16:48 -0500

    progress

--- hangman.py
+++ hangman.py
@@ -2,38 +2,65 @@ import random
 
 from words import words
 
-# print(words)
 
 word_choice = random.choice(words)
+print(word_choice)
+
 
 def split(word):
-    return [char for char in word] * "*"
+    return [char for char in word]
+
+word_split = split(word_choice)
+
+print(word_choice.count('e'))
+
+posit = []
+for char in range(len(word_split)):
+    if word_split[char] == "e":
+        posit.append(char)
+        print(posit)
+    else: 
+        pass
 
 
-# show censored word
-# if a user gets a character right, reveal the character within 
-# censored word
-# otherwise, keep user guessing till length of the word
 
-newword = split(word_choice)
-print(split(word_choice))
+# guess = input("guess a letter")
+
+# for char in word_choice:
 
 
-censoredword = len(word_choice) * "*"
-# print(censoredword)
 
-user = input("guess one letter in the word: ")
 
-i = 0
-cword = []
-while i < len(word_choice):
-    if user in newword:
-        i+= 1 
-        print(user)
-    else:
-        i+= 1
-        cword += "*"
-        print(cword)
+# word_choice = random.choice(words)
+
+# def split(word):
+#     return [char for char in word] 
+
+
+# # show censored word
+# # if a user gets a character right, reveal the character within 
+# # censored word
+# # otherwise, keep user guessing till length of the word
+
+# newword = split(word_choice)
+# print(split(word_choice))
+
+
+# censoredword = len(word_choice) * "*"
+# # print(censoredword)
+
+# user = input("guess one letter in the word: ")
+
+# i = 0
+# cword = []
+# while i < len(word_choice):
+#     if user in newword:
+#         i+= 1 
+#         print(user)
+#     else:
+#         i+= 1
+#         cword += "*"
+#         print(cword)
         # print(censoredword)
 
 # for char in word_choice:
