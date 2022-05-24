09800c15db3cdaae0595ec4f402dd0b0384e4d43
kyotobear <kiytobear@gmail.com>
2022-05-24 16:25:08 -0500

    update code

---
+++ recursion.py
@@ -0,0 +1,38 @@
+# def rec_count(number):
+#     print(number)
+#     #base case
+#     if number == 0:
+#         return 0
+#     rec_count(number - 1) # A recursive call with a different argument
+#     print(number)
+
+
+# rec_count(5)
+
+
+
+
+
+# Fibonacci sequence
+# 0 1 1 2 3 5 8 13
+
+from tkinter.font import names
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+ 
+
+nterms = 7
+
+# check if the number of terms is valid
+if nterms <=0:
+    print("please enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for i in range(nterms):
+        print(fibonacci(i))
+
