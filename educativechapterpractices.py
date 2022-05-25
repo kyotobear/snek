a1abf2429314cbf7c65bed6f45b193e606cb0486
kyotobear <kiytobear@gmail.com>
2022-05-25 17:00:41 -0500

    update code

--- educativechapterpractices.py
+++ educativechapterpractices.py
@@ -171,3 +171,19 @@ should be public.
 This problem can be broken down into 
 two tasks:
 """
+
+class Point:
+
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    
+    def sqSum(self):
+        return (self.x **2 ) + (self.y ** 2) + (self.z ** 2)
+
+
+obj1  = Point(1,3,5)  
+
+print(obj1.sqSum())
