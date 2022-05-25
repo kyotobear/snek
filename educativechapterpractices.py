63e8cbc0a713a1a42216c489313c84d6d9e9fcaa
kyotobear <kiytobear@gmail.com>
2022-05-25 12:29:19 -0500

    update code

--- educativechapterpractices.py
+++ educativechapterpractices.py
@@ -145,3 +145,19 @@ def fib(n):
     
 
 print(fib(7))
+
+
+# outside solution
+def fib(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
