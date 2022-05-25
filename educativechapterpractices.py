f4738cece750c57fa912c1e1900cb9ab921b1e21
kyotobear <kiytobear@gmail.com>
2022-05-24 21:39:22 -0500

    update code

--- educativechapterpractices.py
+++ educativechapterpractices.py
@@ -31,12 +31,49 @@
 
 # factorial(n) = n * (n-1) * (n-2) * 
 
-def factorial(n):
-    if n < 0:
-        return -1
-    elif n <= 1:
-        return n
-    else:
-        return n * factorial(n - 1)
+# def factorial(n):
+#     if n < 0:
+#         return -1
+#     elif n <= 1:
+#         return n
+#     else:
+#         return n * factorial(n - 1)
 
-print(factorial(5))
+# print(factorial(5))
+
+
+# def grid_paths(n,m):
+#     if n == 1 or m == 1:
+#         return 1 
+#     else:
+#         return grid_paths(n, m - 1) + grid_paths(n - 1, m)
+
+# write a function that counts
+# the number of ways you can partition/divide n objects
+# using parts up to m (assuming m >= 0)
+
+# def count_partitions(n,m):
+#     if n == 0:
+#         return 1
+#     elif m == 0 or n < 0:
+#         return 0
+#     else:
+#         return count_partitions(n-m, m) + count_partitions(n, m-1)
+
+
+# Exercise: Balanced Brackets
+"""Given a string containing only square brackets,
+ [], you must check whether the brackets are balanced or not. The brackets are said to be balanced if, 
+ for every opening bracket, there is a closing bracket.
+
+You will write your code in the check_balance() function, 
+which returns True if the brackets are balanced and False
+ if they are not.
+
+ For an empty string, the function will return True.
+"""
+
+def check_balance(brackets):  # The argument is a string
+    pass  # Replace with your code
+
+
