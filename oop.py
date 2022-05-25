549248221962f5afde8acf4027006d192e0d50eb
kyotobear <kiytobear@gmail.com>
2022-05-25 14:41:54 -0500

    update code

--- oop.py
+++ oop.py
@@ -83,3 +83,31 @@ Steve = Employee(3789, 2500, "Human Resources")
 print("ID :", Steve.ID)
 print("Salary :", Steve.salary)
 print("Department :", Steve.department)
+
+
+"""Below is an example where one Employee class 
+object is created without the initializer parameters 
+and one is created with the initializer parameters.
+"""
+
+class Employee:
+    # defining the properties and assigning None to them
+    def __init__(self, ID=None, salary=0, department=None):
+        self.ID = ID
+        self.salary = salary
+        self.department = department
+
+
+# creating an object of the Employee class with default parameters
+Steve = Employee()
+Mark = Employee("3789", 2500, "Human Resources")
+
+# Printing properties of Steve and Mark
+print("Steve")
+print("ID :", Steve.ID)
+print("Salary :", Steve.salary)
+print("Department :", Steve.department)
+print("Mark")
+print("ID :", Mark.ID)
+print("Salary :", Mark.salary)
+print("Department :", Mark.department)
