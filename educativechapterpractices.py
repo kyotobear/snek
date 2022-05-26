8ae15462d4fa8e3a27abab2d41d9be313d3cb55d
kyotobear <kiytobear@gmail.com>
2022-05-25 20:07:44 -0500

    update code

--- educativechapterpractices.py
+++ educativechapterpractices.py
@@ -226,3 +226,23 @@ class Student:
 
     def percentage(self):
         return((self.totalObtained()/300) * 100)
+
+
+
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return (self.num1 + self.num2)
+
+    def subtract(self):
+        return -(self.num1 - self.num2)
+
+    def multiply(self):
+        return (self.num1 * self.num2)
+
+    def divide(self):
+        return (self.num2/self.num1)
+        
