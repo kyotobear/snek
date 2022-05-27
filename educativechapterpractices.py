edf92bb59e537fb7aa61be60a7a5efe40cd08e1c
kyotobear <kiytobear@gmail.com>
2022-05-27 13:44:25 -0500

    c

--- educativechapterpractices.py
+++ educativechapterpractices.py
@@ -250,18 +250,36 @@ class Calculator:
 
 # encapsulation example
 # where the properties are private but are able to be referred with methods
-class Rectangle:
-    def __init__(self, length, width):
-        self.__length = length
-        self.__width = width
+# class Rectangle:
+#     def __init__(self, length, width):
+#         self.__length = length
+#         self.__width = width
 
-    def area(self):
-        return (self.__length * self.__width)
+#     def area(self):
+#         return (self.__length * self.__width)
     
-    def perimeter(self):
-        return (2 * (self.__length + self.__width))
+#     def perimeter(self):
+#         return (2 * (self.__length + self.__width))
 
 
-obj1 = Rectangle(4,5)
-print("the area is: ", obj1.area())
-print("the perimeter is: ", obj1.perimeter())
+# obj1 = Rectangle(4,5)
+# print("the area is: ", obj1.area())
+# print("the perimeter is: ", obj1.perimeter())
+
+
+
+#getters and setters
+# class Student:
+#     def setName(self, name):
+#         self.__name = name
+
+    
+#     def getName(self):
+#         return (self.__name)
+    
+#     def setRollNumber(self,rollNumber):
+#         self.__rollNumber = rollNumber
+
+    
+#     def getRollNumber(self):
+#         return (self.__rollNumber)
