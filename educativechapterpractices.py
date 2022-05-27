ce33d899e79a71239440259f668c17c92a25fc69
kyotobear <kiytobear@gmail.com>
2022-05-27 13:45:54 -0500

    c

--- educativechapterpractices.py
+++ educativechapterpractices.py
@@ -283,3 +283,11 @@ class Calculator:
     
 #     def getRollNumber(self):
 #         return (self.__rollNumber)
+
+# demo1 = Student()
+# demo1.setName("Alex")
+# print("Name:", demo1.getName())
+# demo1.setRollNumber(3789)
+# print("Roll Number:", demo1.getRollNumber())
+
+#Output: Name: Alex Roll Number: 3789
