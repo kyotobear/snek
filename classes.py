38efcd6a7959e7c9276f89d5210a5fa872128705
kyotobear <kiytobear@gmail.com>
2022-06-05 20:35:17 -0500

    c

--- classes.py
+++ classes.py
@@ -9,8 +9,14 @@ class Robot:
         self.color = color
         self.weight = weight    
 
+# when function is a method of a class - add additional argumnent self 
+# add self to every method in the class
     def introduce_self(self):
-        print("My name is " + self.name + " and I am " + self.color + " and I weigh "  + self.weight + " pounds")
+        return print("My name is " + self.name + " and I am " + self.color + " and I weigh "  + str(self.weight) + " pounds")
 
 
-        
+obj1 = Robot("Nellie", "pink", 10)
+obj2 = Robot("Tommie", "silver", 12)
+
+print(obj1.introduce_self())
+print(obj2.introduce_self())
